1b67f37c392d4deb25cbeed3f2e3f723e3cf6d27
Igor <igoralbuquerque021@gmail.com>
2024-04-30 13:38:21 -0300

    feat: API Started

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("Rodando")
+import express from 'express'
+import config from 'config'
+import router from './router'
+
+const app = express();
+const port = config.get<number>('port');
+
+app.use(express.json());
+
+app.use("/api", router);
+
+app.listen(port, ()=>{
+    console.log(`rodando na porta ${port}`);
+})
